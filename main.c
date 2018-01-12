1cad800121c07bda8de966f125bd7bc42a3fdd10
codebender828 <jbakebwa@163.com>
2018-01-12 19:40:04 +0800

    Includes HuffMan Trees in C

--- main.c
+++ main.c
@@ -12,23 +12,49 @@
 #define ERROR 0
 #define INFEASIBLE -1
 
+#define MAX_NUM_NODES 100
+
 typedef int Status;
 typedef int Boolean;
 typedef int TElemType;
 
-
+//二叉树定义
 typedef struct BiTNode{
     TElemType data;
     struct BiTNode *lchild;
     struct BITNode *rchild;
 }BiTNode, *BiTree;
 
+//三叉树
 typedef struct TriTNode{
     TElemType data;
     struct TriTNode *lchild;
     struct TriTNode *rchild;
     struct TriTNode *parent;
 }TriTNode, *TriTree;
+
+
+//双亲链表
+typedef struct BTPNode{
+    TElemType data;
+    int btparent;
+    char LRTag;
+}BTPNode;
+
+typedef struct BTPTree{
+    BTPNode nodes[MAX_NUM_NODES];
+    int num_node;
+    int root;
+};
+
+//哈夫曼树定义  ------------------    注意：静态三叉链表类型定义
+typedef struct {
+    unsigned int weight;
+    unsigned int parent;
+    unsigned int lchild;
+    unsigned int rchild;
+}HTNode, *HuffmanTree;
+
 
 
 
